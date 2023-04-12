c9d62051ae917af8c567fd30e203102b15a33324
goalslee <goals1025@gmail.com>
2023-04-12 14:24:25 +0900

    programmers update

--- Programmers/ProgrammersPython/삼각형의완성조건(1).py
+++ Programmers/ProgrammersPython/삼각형의완성조건(1).py
@@ -1,9 +1,9 @@
 def solution(sides):
     answer = 0
     sides.sort()
-    if sides[2] >= sides[0] + sides[1]:
-        answer = 2
-    else:
+    if sides[2] < sides[0] + sides[1]:
         answer = 1
+    else:
+        answer = 2
         
     return answer
